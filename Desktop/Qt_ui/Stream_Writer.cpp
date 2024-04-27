d4ccca65251609052e661eff44db04e355866c93
紫云 <1583931339@qq.com>
2024-04-27 23:45:35 +0800

    cpp

---
+++ Desktop/Qt_ui/Stream_Writer.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+#include <ifstream>
+#include "Obtainer.cpp"
+
+int writer(){
+
+    std::cout << "test" << std::endl;
+    ifstream  file;
+    file.open("file.dat", ios::out | ios::in );
+
+}
+
